6b50d1984ab0e42cb3327d5a1c217ac48e276ca9
Emma Nasseri <emmanasseri@Emmas-MacBook-Air.local>
2024-06-20 20:39:48 -0500

    added week 3 lecture slides

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -12,6 +12,14 @@ import {
 
 const class_lectures = [
   {
+    title: "Week 2 - Applications of Blockchain",
+    date: "June 20, 2024",
+    embed:
+      "<iframe id='kmsembed-1_jorrrtyg' width='500' height='385' src='https://mediahub.ku.edu/embed/secure/iframe/entryId/1_jorrrtyg/uiConfId/46527243/st/0' class='kmsembed' allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' referrerPolicy='no-referrer-when-downgrade' sandbox='allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation' frameborder='0' title='Kansas Blockchain Fellowship Week 0 - Orientation'></iframe>",
+    lecture_notes:
+      "https://docs.google.com/presentation/d/1NNgdhRQx8bwZ-ukPUasiqAQSi2D0ygvR2zpsZMoUyFk/edit?usp=sharing",
+  },
+  {
     title: "Week 1 - Basics of Blockchain",
     date: "June 13, 2024",
     embed: "<iframe id='kmsembed-1_isz4j0s4' width='500' height='385' src='https://mediahub.ku.edu/embed/secure/iframe/entryId/1_isz4j0s4/uiConfId/45147491/st/0' class='kmsembed' allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' referrerPolicy='no-referrer-when-downgrade' sandbox='allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation' frameborder='0' title='Kansas Blockchain Fellowship Week 0 - Orientation'></iframe>",
@@ -25,6 +33,7 @@ const class_lectures = [
     lecture_notes:
       "https://docs.google.com/presentation/d/19UXVcwlc6-OJJ4ZcGLMpC977YSyab2QxtTrYilTyHxk/edit?usp=sharing",
   },
+  
 ];
 
 const supplemental_materials = [
