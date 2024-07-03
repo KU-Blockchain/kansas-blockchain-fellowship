569c386d802cf3d7fcf7c4fb590e05a6e88557e0
Micah Borghese <micahborghese@gmail.com>
2024-07-03 15:32:26 -0500

    add week 4

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -12,6 +12,14 @@ import {
 
 const class_lectures = [
   {
+    title: "Week 4 - Overview of Industry",
+    date: "July 2, 2024",
+    embed:
+      "<iframe id='kmsembed-1_srjfr4sl' width='500' height='385' src='https://mediahub.ku.edu/embed/secure/iframe/entryId/1_srjfr4sl/uiConfId/45147491/st/0' class='kmsembed' allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' referrerPolicy='no-referrer-when-downgrade' sandbox='allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation' frameborder='0' title='Kansas Blockchain Fellowship - Week 4'></iframe>",
+    lecture_notes:
+      "https://docs.google.com/presentation/d/1ytGCSaOMSnIqeFuVfzgFTJC0SL7odW8mdmTi_CN_-Dg/edit?usp=sharing",
+  },
+  {
     title: "Week 3 - DeFi + Regulation",
     date: "June 27, 2024",
     embed:
